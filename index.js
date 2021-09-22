cb115c77df32437df9787355115589a98ff5885c
Mehmet-Kaan <82370842+Mehmet-Kaan@users.noreply.github.com>
2021-09-22 15:13:49 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 async function getUsers(){
-    let response = await fetch("http://mpp.erikpineiro.se/dbp/sameTaste/users.php");
+    let response = await fetch("http://mpp.erikpineiro.se/dbp/users/");
     let data = await response.json();
     return data;
 }
 
 getUsers()
 .then(data => {
-    data.message.forEach(user => {
-      let p = document.createElement("p");
-        p.innerText = user.alias;
+    console.log(data);
+    // data.message.forEach(user => {
 
-        document.body.append(p);
-    });
+    //   let p = document.createElement("p");
+    //     p.innerText = user.alias;
+
+    // //     document.body.append(p);
+    // });
 })
